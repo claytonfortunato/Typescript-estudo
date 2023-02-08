a54e6e4c0d267c6c33bf58a03cf664001a7cb9f4
claytonfortunato <claytonreis10@gmail.com>
2023-02-07 23:04:55 -0300

    feat: filtrar

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
+import { v4 } from "uuid";
 
 import "./styles.scss";
 
@@ -9,24 +10,100 @@ interface ITodo {
 }
 
 function App() {
+  const [todo, setTodo] = useState<ITodo[]>([
+    {
+      descricao: "comprar pão",
+      completo: false,
+      id: "123456",
+    },
+  ]);
+  const [descricao, setDescricao] = useState<string>("");
+  const [filtros, setFiltros] = useState<"todos" | "paraFazer" | "completos">(
+    "todos"
+  );
+
+  const handleTodo = (id: string) => {
+    setTodo((prev) => {
+      const novoValor = prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completo: !todo.completo };
+        }
+        return todo;
+      });
+      return novoValor;
+    });
+  };
+
+  const adicionarTodo = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!descricao) return alert("Favor preender a descrição!");
+
+    setTodo((prev) => [
+      ...prev,
+      {
+        descricao: descricao,
+        completo: false,
+        id: v4(),
+      },
+    ]);
+    setDescricao("");
+  };
+
+  const filtraTodos = (filtro: string) => {
+    if (filtro === "all") return todo;
+    if (filtro === "paraFazer")
+      return todo.filter((todo) => todo.completo === false);
+    if (filtro === "completos")
+      return todo.filter((todo) => todo.completo === true);
+  };
+
   return (
     <div className="App">
       <header>Todo List</header>
       <main className="container">
         <nav>
-          <li>Todos</li>
-          <li>Para fazer</li>
-          <li>Completos</li>
+          <li
+            onClick={() => setFiltros("todos")}
+            className={`${filtros === "todos" ? "filtroAtivo" : ""}`}
+          >
+            Todos
+          </li>
+          <li
+            onClick={() => setFiltros("paraFazer")}
+            className={`${filtros === "paraFazer" ? "filtroAtivo" : ""}`}
+          >
+            Para fazer
+          </li>
+          <li
+            onClick={() => setFiltros("completos")}
+            className={`${filtros === "completos" ? "filtroAtivo" : ""}`}
+          >
+            Completos
+          </li>
         </nav>
-        <form>
-          <input type="text" placeholder="Adicionar detalhes" />
+        <form onSubmit={adicionarTodo}>
+          <input
+            type="text"
+            placeholder="Adicionar detalhes"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+          />
           <button>Adicionar</button>
         </form>
         <section className="todo-container">
-          <div className="todo">
-            <input type="checkbox" id="comprar pão" />
-            <label htmlFor="comprar pão">comprar pão</label>
-          </div>
+          {todo &&
+            filtraTodos(filtros)?.map(({ id, descricao, completo }) => (
+              <div className="todo" key={id}>
+                <input
+                  type="checkbox"
+                  id={id}
+                  checked={completo}
+                  onChange={() => handleTodo(id)}
+                />
+                <label htmlFor={id}>{descricao}</label>
+              </div>
+            ))}
         </section>
       </main>
     </div>
